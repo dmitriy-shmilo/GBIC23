f16f74b9f1aac147e78a5eb99feafed932739785
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2023-07-06 00:49:31 +0300

    Blink when telegraphing attacks.

--- enemy/state/enemy_state_attack_charging.gd
+++ enemy/state/enemy_state_attack_charging.gd
@@ -1,18 +1,20 @@
 class_name EnemyStateAttackTelegraph
 extends State
 
+const blink_shader: ShaderMaterial = preload("res://base/shader/blink.tres")
+
 @export var telegraph_time: float = 1.0
 @export var sprite: Node2D = null
 
 var _current_telegraph_time: float = 0.0
 
 func enter(args: Dictionary = {}) -> void:
-	_current_telegraph_time = 0.0
-	sprite.modulate = Color.ORANGE_RED
+	sprite.material = blink_shader
 
 
 func exit() -> void:
-	sprite.modulate = Color.WHITE
+	sprite.material = null
+	_current_telegraph_time = 0.0
 
 
 func physics_process(delta: float) -> void:
