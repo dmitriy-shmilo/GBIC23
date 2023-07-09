7db30497e30d785d91d545824643bbe35e164d9d
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2023-07-09 21:50:50 +0300

    Add portal scene.

---
+++ map/portal.gd
@@ -0,0 +1,8 @@
+class_name Portal
+extends Node2D
+
+@onready var _animation_player: AnimationPlayer = $"AnimationPlayer"
+
+
+func _on_go_home_interactive_target_changed(interactor, targets) -> void:
+	_animation_player.play("highlight" if targets else "RESET")
