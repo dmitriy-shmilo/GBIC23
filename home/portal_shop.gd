3cad48a97c3a664105644f86d91871c15b6d590e
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2023-07-11 16:43:00 +0300

    Add gossip to the portal shop.

--- home/portal_shop.gd
+++ home/portal_shop.gd
@@ -6,6 +6,7 @@ extends HubShop
 @onready var _portal_buy_button2: BetterButton = $"BuyPortalMenu/MarginContainer/BuyPortalMenu/BuyPortal2/PortalBuyButton"
 @onready var _portal_buy_button3: BetterButton = $"BuyPortalMenu/MarginContainer/BuyPortalMenu/BuyPortal3/PortalBuyButton"
 @onready var _portal_buy_button4: BetterButton = $"BuyPortalMenu/MarginContainer/BuyPortalMenu/BuyPortal4/PortalBuyButton"
+@onready var _text_label = $"TextPanel/TextLabel"
 
 @onready var _portal_buy_buttons: Array[BetterButton] = [
 	_portal_buy_button1,
@@ -21,6 +22,14 @@ var _portal_costs = [
 	150
 ]
 
+var _last_gossip = 0
+var _gossip = [
+	"gossip_portal_1",
+	"gossip_portal_2",
+	"gossip_portal_3",
+	"gossip_portal_4",
+	"gossip_portal_5",
+]
 
 func _ready() -> void:
 	super._ready()
@@ -34,3 +43,9 @@ func _on_portal_buy_button_pressed(button: BetterButton) -> void:
 	var cost = _portal_costs[button.tag]
 	SaveManager.data.money -= cost
 	get_tree().change_scene_to_file("res://main.tscn")
+
+
+func _on_portal_gossip_button_pressed() -> void:
+	var text = tr(_gossip[_last_gossip])
+	_text_label.text = text
+	_last_gossip = (_last_gossip + 1) % _gossip.size()
