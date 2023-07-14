e6401e3c24498758c2e236e423f62e3d50a53742
Dmitriy Shmilo <dmitriy.shmilo@gmail.com>
2023-07-14 14:27:34 +0300

    Display a portal ghost when the portal is not on screen.

--- gui/hud.gd
+++ gui/hud.gd
@@ -1,20 +1,30 @@
 class_name Hud
 extends Control
 
+const MAX_ICONS = 10
+
 @export var vitals: VitalsComponent = null
 @export var inventory: InventoryComponent = null
+@export var portal: Portal = null
+@export var player: Player = null
 
 @onready var _health_container: HBoxContainer = $"%HealthContainer"
 @onready var _extra_health_label = $"%HealthContainer/ExtraHealthLabel"
 @onready var _health_icon_template = $"%HealthIconTemplate"
 @onready var _inventory_capacity_label = $"InventoryCapacityLabel"
+@onready var _portal_indicator = $"PortalIndiciator"
 
-const MAX_ICONS = 10
+var _screen_size = Vector2.ZERO
+var _padded_size = Vector2.ZERO
 
 func _ready() -> void:
-	vitals.connect("health_changed", _on_vitals_health_changed)
-	inventory.changed.connect(_on_inventory_changed)
+	_screen_size = get_viewport_rect().size / 2
+	_padded_size = Vector2(_screen_size.x - 16, _screen_size.y - 16)
 
+	vitals.health_changed.connect(_on_vitals_health_changed)
+	inventory.changed.connect(_on_inventory_changed)
+	portal.screen_entered.connect(_on_portal_screen_entered)
+	portal.screen_exited.connect(_on_portal_screen_exited)
 	for i in range(MAX_ICONS):
 		var heart_rect = _health_icon_template.duplicate(0)
 		heart_rect.visible = true
@@ -22,6 +32,12 @@ func _ready() -> void:
 	_extra_health_label.move_to_front()
 	_refresh_health(vitals)
 	_on_inventory_changed(inventory)
+
+
+func _physics_process(_delta: float) -> void:
+	var portal_pos = portal.global_position - player.global_position
+	_portal_indicator.position = portal_pos.clamp(-_padded_size,
+		_padded_size) + _screen_size
 
 
 func _refresh_health(new_vitals: VitalsComponent) -> void:
@@ -42,3 +58,11 @@ func _on_vitals_health_changed(new_vitals: VitalsComponent, _positive: bool) -> 
 
 func _on_inventory_changed(_inventory: InventoryComponent) -> void:
 	_inventory_capacity_label.text = "%d/%d" % [inventory.items.size(), inventory.max_items]
+
+
+func _on_portal_screen_entered() -> void:
+	_portal_indicator.visible = false
+
+
+func _on_portal_screen_exited() -> void:
+	_portal_indicator.visible = true
--- map/portal.gd
+++ map/portal.gd
@@ -1,6 +1,9 @@
 class_name Portal
 extends Node2D
 
+signal screen_entered()
+signal screen_exited()
+
 @onready var _animation_player: AnimationPlayer = $"AnimationPlayer"
 @onready var _inventory: InventoryComponent = $"InventoryComponent"
 
@@ -11,3 +14,11 @@ func _ready() -> void:
 
 func _on_go_home_interactive_target_changed(_interactor, targets) -> void:
 	_animation_player.play("highlight" if targets else "RESET")
+
+
+func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
+	screen_entered.emit()
+
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	screen_exited.emit()
